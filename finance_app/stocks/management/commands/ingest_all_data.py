34c9e40ad6ce0bc253b49f8434448084e48e162a
Mihail Gjorgjievski <mihailgjorgjievski@yahoo.com>
2025-03-25 18:27:55 +0100

    Added CLI interfaces to populate database

--- finance_app/stocks/management/commands/ingest_all_data.py
+++ finance_app/stocks/management/commands/ingest_all_data.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from stocks.tasks import batch_ingest_all_symbols
 
+# sudo docker compose exec web python manage.py ingest_all_data
+# Calls neccessery functionalities to collect and store data for all companies in the database
+
 class Command(BaseCommand):
     help = "Triggers historical ingestion for all S&P 500 stocks (via Celery)"
 
--- finance_app/stocks/management/commands/ingest_sp500.py
+++ finance_app/stocks/management/commands/ingest_sp500.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand
 from stocks.services.ingestion import store_sp500_symbols
 
+# sudo docker compose exec web python manage.py ingest_sp500
+# Calls the functions to collect all of the S&P 500 company symbols (TICKERS)
+
+
 class Command(BaseCommand):
     help = "Ingests current S&P 500 stock symbols into the database"
 
